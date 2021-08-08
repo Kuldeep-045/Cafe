accbfaa150389154abcfd6d9c96db156d6742c02
Kuldeep-045 <86959046+Kuldeep-045@users.noreply.github.com>
2021-08-08 21:33:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+
+function slideshow()
+{
+    var x=document.getElementById('check-class');
+    if(x.style.display==="none")
+    {
+        x.style.display="block";
+        // alert("hi");
+    }
+    else {
+        x.style.display="none";
+    }
+
+}
